3d257bc1640dcb20affc9ced55b2be2ee58c9057
Juan <juanpontonrodri@gmail.com>
2022-10-24 14:46:53 +0200

    i

--- p1.js
+++ p1.js
@@ -1,16 +1,49 @@
+
+function validacion(){
+
+  if(checkdni()==false){
+    let text1="DNI is not valid. Correct format:12345678A"
+    return false;
+  }
+
+
+  alert(text1);
+}
+
+function checkall(){
+  if(document.getElementById("cgenreall").checked==true){
+    var boxes=document.getElementsByName("cgenre");
+    
+    array.forEach(element => {
+      element.checked=true;
+    });
+}}
+
+function uncheckall(){
+  console.log("uncheckall");
+  if(document.getElementById("cgenreall").checked==true){
+    var boxes=document.getElementsByName("cgenre");
+    for(var i=0;i<boxes.length;i++){
+      boxes[i].checked=false;
+    }
+    
+  }
+}
+
+
+
 function get_function(){
     document.getElementById('multipart').disabled = true; /Si uso get no puedo usar multipart/
-    document.getElementById('registration_form').method="get";
-    document.getElementById('registration_form').enctype="application/x-www-form-urlencoded";
+    document.getElementById('reg_form').method="get";
+    document.getElementById('reg_form').enctype="application/x-www-form-urlencoded";
 }
 
 
 function post_function(){
     document.getElementById('multipart').disabled = false;
-    document.getElementById('registration_form').method="post";
-    document.getElementById('registration_form').enctype="multipart/form-data";
+    document.getElementById('reg_form').method="post";
+    document.getElementById('reg_form').enctype="multipart/form-data";
 }
-
 
 /*
 function checkformats() {
@@ -26,11 +59,13 @@ function checkformats() {
 */
 
 function checkdni(){
-    cdni=document.getElementByName('cdni');
+    console.log("cheking")
+    cdni=document.getElementsByName('cdni');
     var numeros = /^[0-7]{1}[0-9]{7}[A-Za-z]{1}$/;
     var letra= /[a-zA-Z]/;
     if((cdni="") || ((numeros.test(cdni.substring(0,cdni.lenght-1))==true) && (letra.test(cdni.charAt(cdni.length-1))==true))){
-        
+        return true;
     }
-
+    else 
+    return false;
 }
